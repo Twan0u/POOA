387a31993a013ebc02ee72fc171e5e357633711d
Twan0u <ant.lamb.al@gmail.com>
2019-04-08 21:34:11 +0200

    Création de l'objet Client et de ses dépendances

--- Client.java
+++ Client.java
@@ -6,12 +6,67 @@ dans le cadre du cours de Programation avancée Orientée Objets
 
 public class Client {
   /*Variables d'instances*/
-  //
+  static int counterNumber; // A VERIFIER
+  private int number;
+  private String name;
+  private String phoneNumber;
+  private double discount;
+  private String VATNumber; // Facultatif
+
 
 
 /*Methode constructeur pour les objets Client*/
-  public Client(){
-// ajouter en argument tous les paramètres
+  public Client(int number, String name, String phoneNumber, double discount, String VATNumber){
+    setNumber(number);
+    setName(name);
+    setPhoneNumber(phoneNumber);
+    setDiscount(discount);
+    setVATNumber(VATNumber);
   }
-//Getteurs et setteurs des varaibles qui doivent être modifiées
+  public Client(int number, String name, String phoneNumber, double discount){
+    client(number,name,phoneNumber,discount,null);
+  }
+  public Client(int number, String name, String phoneNumber){
+    client(number,name,phoneNumber,0,null);
+  }
+
+  /* GETTEURS */
+  public getNumber(){
+    return this.number;
+  }
+  public getName(){
+    return this.name;
+  }
+  public getPhoneNumber(){
+    return this.phoneNumber;
+  }
+  public getDiscount(){
+    return this.discount;
+  }
+  public getVATNumber(){
+    return this.VATNumber;
+  }
+
+
+  /* SETTEURS */
+  private setNumber(int num){
+    // Verification et incrémentation ????
+  }
+  public setName(String name){
+    //verification ( taille maximum )
+    this.name = name;
+  }
+  public setPhoneNumber(String phone){
+    // Verification
+    this.phoneNumber = phone;
+  }
+  public setDiscount(double discount){
+    // VERIFIER
+    this.discount = discount;
+  }
+  public setVATNumber(String vatNum){
+    //VERIFIER
+    this.VATNumber = vatNum;
+  }
+
 }
