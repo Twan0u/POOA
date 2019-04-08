6d7a3012b734ac25099de13deb7359e82243c3bc
Twan0u <ant.lamb.al@gmail.com>
2019-04-08 22:50:29 +0200

    business unit crée

--- BusinessUnit.java
+++ BusinessUnit.java
@@ -4,14 +4,66 @@ Programme de Gestion d'une brasserie
 dans le cadre du cours de Programation avancée Orientée Objets
 */
 
-public class BusinessUnit {
+public class BusinessUnit{
   /*Variables d'instances*/
-  //
-
+  private static int counterid; // A VERIFIER
+  private int idBusinessUnit;
+  private Client client;
+  private Locality locality;// Tableau des différentes localisations de l'entreprise
+  private String streetName;
+  private String streetNumber;
 
 /*Methode constructeur pour les objets Client*/
-  public BusinessUnit(){
-// ajouter en argument tous les paramètres
+  public BusinessUnit(int id, Client client, Locality locality, String streetName, String streetNumber){
+    setIdBusinessUnit(id);
+    setClient(client);
+    setLocality(locality);
+    setStreetName(streetName);
+    setStreetNumber(streetNumber);
   }
-//Getteurs et setteurs des varaibles qui doivent être modifiées
+
+  /* GETTEURS */
+  public int getIdBusinessUnit(){
+    return this.idBusinessUnit;
+  }
+  public Client getClient(){
+    return this.client;
+  }
+  public Locality getLocality(){
+    return this.locality;
+  }
+  public String getStreetName(){
+    return this.streetName;
+  }
+  public String getStreetNumber(){
+    return this.streetNumber;
+  }
+
+  /* SETTEURS */
+  private void setIdBusinessUnit(int id){
+    //VERIFIER
+    this.idBusinessUnit = id;
+  }
+  private void setClient(Client client){
+    //VERIFIER
+    this.client = client;
+  }
+  private void setLocality(Locality locality){
+    //VERIFIER
+    this.locality = locality;
+  }
+  private void setStreetName(String streetName){
+    //VERIFIER
+    this.streetName = streetName;
+  }
+  private void setStreetNumber(String streetNumber){
+    //VERIFIER
+    this.streetNumber = streetNumber;
+  }
+
+
+  /* Utils */
+  public String toString(){
+    return getIdBusinessUnit() + " - " + client.toString() + " - " + locality.toString() + " - " + getStreetName() + " - " + getStreetNumber();
+  }
 }
