19c456bd7961d68120d3bbcdaa9161dc3df14c0f
Twan0u <ant.lamb.al@gmail.com>
2019-05-13 09:43:11 +0200

    Merci de garder vos beta tests en privé ...

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,6 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-      //new  OrderGui();
-        InterfaceData dbAccess = new DBAccess();
-        try{
-            ArrayList<Beer> beers = dbAccess.getAllBeers();
-            for(Beer beer : beers){
-                System.out.println(beer);
-            }
-        }
-        catch(Exception e){
-            System.out.println("Echec dans le main");
-            System.exit(0);
-        }
-
+      new  OrderGui();
     }
 }
