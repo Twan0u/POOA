48761fd8ebd7c9d52b3f16e226be6c6300e49887
Nathan Surquin <nsurquin@outlook.be>
2019-08-14 12:12:00 +0200

    debut stats

--- src/gui/OrdersToDeliver.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-
-public class OrdersToDeliver extends JPanel{
-    //Order orders
-}
---
+++ src/gui/StatsPanel.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javax.swing.*;
+
+import composants.StatsOnOrders;
+import controller.*;
+import exceptions.ProgramErrorException;
+
+public class StatsPanel extends JPanel {
+    private Controller controller;
+    private JLabel statsOnBeersLabel;
+    private JLabel orderAvgValue;
+    private JTable tableStats;
+
+    StatsOnOrders stats;
+
+    public StatsPanel(){
+        try {
+            stats = controller.getStatsOnOrders();
+        }
+        catch(ProgramErrorException e){
+            JOptionPane.showMessageDialog (null,"Oups, une erreur s'est produite lors de la récupération de stats sur les commnandes","ERREUR", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        orderAvgValue = new JLabel("Valeur moyenne des commandes : " + stats.getOrderAvgValue());
+        
+    }
+}
