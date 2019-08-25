99917493603538ef09261c87c8849b552b7a92aa
Nathan Surquin <nsurquin@outlook.be>
2019-08-25 12:59:57 +0200

    push useless mais qd meme utile pr moi

--- src/gui/CatchMeThread.java
+++ src/gui/CatchMeThread.java
@@ -15,29 +15,29 @@ public class CatchMeThread extends Thread{
     Color[] couleurs={Color.red,Color.green,Color.blue,Color.pink,Color.orange,Color.cyan,Color.black,Color.yellow};
 
     public CatchMeThread(){
-      frame = new JFrame();
-      frame.pack();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-      height = screenSize.height;
-      width = screenSize.width;
-      frame.setSize(width/10, height/10);
-      label = new JLabel("Félicitation");
-      frame.add(label);
-      frame.setVisible(true);
+        frame = new JFrame();
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        height = screenSize.height;
+        width = screenSize.width;
+        frame.setSize(width/10, height/10);
+        label = new JLabel("Félicitation");
+        frame.add(label);
+        frame.setVisible(true);
     }
 
     public void run(){
         while (true){
-          label.setForeground(couleurs[Math.toIntExact(Math.round(Math.random() * 6))]);
-          frame.setBackground(couleurs[Math.toIntExact(Math.round(Math.random() * 6))]);
-          frame.setLocation(Math.toIntExact(Math.round(Math.random() * this.width)),Math.toIntExact(Math.round(Math.random() * this.height)));
-          frame.setVisible(true);
-          try {
-              sleep(50);
-          } catch (InterruptedException error) {
-              JOptionPane.showMessageDialog (null, "Le thread n'est pas fatigué et ne veut pas aller dormir","ERREUR", JOptionPane.ERROR_MESSAGE);
-          }
+            label.setForeground(couleurs[Math.toIntExact(Math.round(Math.random() * 6))]);
+            frame.setBackground(couleurs[Math.toIntExact(Math.round(Math.random() * 6))]);
+            frame.setLocation(Math.toIntExact(Math.round(Math.random() * this.width)),Math.toIntExact(Math.round(Math.random() * this.height)));
+            frame.setVisible(true);
+            try {
+                sleep(50);
+            } catch (InterruptedException error) {
+                JOptionPane.showMessageDialog (null, "Le thread n'est pas fatigué et ne veut pas aller dormir","ERREUR", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
